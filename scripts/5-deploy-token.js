9cb9e5733160183204bb2f514b30cd78df60dc87
Hugo Boutot <hugo.anthonyboutot@gmail.com>
2021-12-23 10:30:17 +0100

    5 add print money

--- scripts/5-deploy-token.js
+++ scripts/5-deploy-token.js
@@ -1,16 +1,16 @@
 import sdk from "./1-initialize-sdk.js";
 
 // In order to deploy the new contract we need our old friend the app module again.
-const app = sdk.getAppModule("INSERT_YOUR_APP_ADDRESS");
+const app = sdk.getAppModule("0xE52b7d655ee9362Faf4F6fAA81303Cd93F9FbB27");
 
 (async () => {
   try {
     // Deploy a standard ERC-20 contract.
     const tokenModule = await app.deployTokenModule({
       // What's your token's name? Ex. "Ethereum"
-      name: "NarutoDAO Governance Token",
+      name: "SKDAO Governance Token",
       // What's your token's symbol? Ex. "ETH"
-      symbol: "HOKAGE",
+      symbol: "SKD",
     });
     console.log(
       "✅ Successfully deployed token module, address:",

8596f1c414e2f6b758d9d381c5eb0286510c2925
Hugo Boutot <hugo.anthonyboutot@gmail.com>
2021-12-25 23:26:48 +0100

    change to skdao

--- src/App.jsx
+++ src/App.jsx
@@ -178,7 +178,7 @@ if (error && error.name === "UnsupportedChainIdError") {
   if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to NarutoDAO</h1>
+        <h1>Welcome to Skind 5 DAO</h1>
         <button onClick={() => connectWallet("injected")} className="btn-hero">
           Connect your wallet
         </button>
